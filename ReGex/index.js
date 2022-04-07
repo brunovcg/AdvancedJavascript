5f8cea012c5ab7f0211a4ae19c2b340b90c20eff
Bruno Gouveia <brunovcg@gmail.com>
2022-04-06 21:53:52 -0300

    commit

--- ReGex/index.js
+++ ReGex/index.js
@@ -1,16 +1,19 @@
-//!   g  - Global (encontra todas as ocorrências)
-//!   i  - insensitive (não checa letras upper x lowercase)
-//!  ( ) - grupos
-//!   |  - ou
-//!   \  - caractere de escape
-//! {n,m} - quantidade de vezes varivael
-//!  {n} - quantidade especifica
-//!   .  - qualquer caractere menos quebra de espace
+//!       g       Global (encontra todas as ocorrências)
+//!       i       insensitive (não checa letras upper x lowercase)
+//!      ( )      grupos
+//!       |       ou
+//!       \       caractere de escape
+//!   {min,max}     quantidade de vezes varivael
+//!      {n}      quantidade especifica
+//!       .       qualquer caractere menos quebra de espace
+//!      []       Conjunto
+//!       ^       usado para negar
+//!    min-max       range
 
 //! ------- FUNÇÕES ------- (Métodos String)
 //!     search - Retorna o índice ou -1 caso não haja
 //!     split - divide o texto usando regex
-//!     match - se usada com a flag g, retora um array com os achados, sem o g volta um array que nem exec
+//!     match - se usada com a flag g, retora um array com os achadossem o g volta um array que nem exec
 //!     replace -  substitui o primeiro param pelo segundo
 
 //! ------- QUANTIFICADORES --------
@@ -18,17 +21,17 @@
 //!    + (obg)  1 ou n
 //!    ? (opc)  0 ou 1
 
-const printAula = 4;
+const printAula = 5;
 
 // ? ------------------------- AULA1 ------------------
 //* exec e test
 
-const texto = `O texto dissertativo é um dos estilos de escrita mais exigidos nos concursos, em vestibulares e outras provas.
-No Exame Nacional do Ensino Médio (Enem), por exemplo, os candidatos são submetidos à uma redação dissertativo-argumentativa, que é um dos tipos de dissertação.
+const texto = `O texto dissertativo é um dos estilos de escrita mais exigidos nos concursosem vestibulares e outras provas.
+No Exame Nacional do Ensino Médio (Enem)por exemploos candidatos são submetidos à uma redação dissertativo-argumentativaque é um dos tipos de dissertação.
 Escrever um bom texto dissertativo exige domínio da língua portuguesa e capacidade de apresentar as informações seguindo uma linha lógica.
-Afinal, o leitor precisa ter clara compreensão sobre o que está sendo transmitido, de forma que não exista ruídos e ou qualquer dificuldade de interpretação.
-É, de fato, um desafio.
-Por isso, no próprio Enem, que São poucos os candidatos QUUE conquistam a nota máxima.`;
+Afinalo leitor precisa ter clara compreensão sobre o que está sendo transmitidode forma que não exista ruídos e ou qualquer dificuldade de interpretação.
+Éde fatoum desafio.
+Por issono próprio Enemque São poucos os candidatos QUUE conquistam a nota máxima.`;
 
 const regExp1 = /(poucos|são)/gi;
 
@@ -108,3 +111,49 @@ console.log(html.match(/<.+>.+<\/.+>/g)) // greedy
 console.log(html.match(/<.+?>.+?<\/.+?>/g)) // não greedy
 
 }
+
+// ? ---------------------------- AULA5 ---------------------------
+//* conjuntos e ranges
+
+// [abc] -> Conjunto 
+// [^abc]-> negacao
+
+const alfa = "abcdefghijklmnopqrstuvwxyz 0123456789 @ ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const alfa2 = "zabcdefghijklmnopqrstuvwxy 0123456789 @®; ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const bruno = "bruno"
+
+if (printAula ===5){
+
+  console.log(alfa)
+  // acha e divide num array cada um
+  console.log(alfa.match(/[abc]/g))
+  //acha em sequencia
+  console.log(alfa.match(/[abc123]+/g))
+  //negando ^
+  console.log(alfa.match(/[^abc123]/g)) 
+
+
+  //range junto
+  console.log(alfa.match(/[0-9]+/g))
+  console.log(alfa.match(/[a-z]+/g))
+  //range separado array
+  console.log(alfa.match(/[0-9]/g))
+  console.log(alfa.match(/[a-z]/g))
+  console.log(alfa2.match(/[a-z]/g))
+  console.log(bruno.match(/[a-z]/g))
+
+  // varios range
+  console.log(alfa.match(/[a-zA-Z0-9]+/g))
+  console.log(alfa.match(/[a-zA-Z0-9]/g))
+
+  console.log(alfa.match(/[^a-zA-Z0-9]+/g)) //negando
+
+
+  //  \w = a-zA-Z0-9
+  console.log(alfa.match(/[\w]/g)) 
+  console.log(alfa.match(/[\u0040-\u0041]+/g)) // unicode
+
+
+
+}
+
